3e86323b02ae22a508911209cb1c8125e9cac708
gideon smith <136742706+gideon-smith@users.noreply.github.com>
2025-02-14 07:16:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from hashlib import sha256
 password = input("password: ")
 thing = input("thing: ")
 password = password + thing
-password = sha256(password.encode('utf-8').hexdigest)
+password = sha256(password.encode('utf-8').hexdigest())
 print('your password is: ' , password)
